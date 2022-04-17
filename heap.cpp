9d857852cbb1bcd28550cbd11d4dd4f2d7133699
AbdelitoCabrera <73550656+AbdelitoCabrera@users.noreply.github.com>
2022-04-17 13:26:51 -0400

    added more info to printTop()

--- heap.cpp
+++ heap.cpp
@@ -58,9 +58,9 @@ class minHeap
     
     void printTop() //missing rest of info
     {
-        cout << "Recipe: " << Heap[0]->getName() << "\n" << "Time to make: " << Heap[0]->getTime() << "\n" ;
-        cout << "Recipe: " << Heap[1]->getName() << "\n" << "Time to make: " << Heap[1]->getTime() << "\n" ;
-        cout << "Recipe: " << Heap[2]->getName() << "\n" << "Time to make: " << Heap[2]->getTime() << "\n" ;
+        cout << "Recipe: " << Heap[0]->getName() << "\n" << "Time to make: " << Heap[0]->getTime() << "\n" << "Calories: " << Heap[0]->getNutrients()[0] << "\n" ;
+        cout << "Recipe: " << Heap[1]->getName() << "\n" << "Time to make: " << Heap[1]->getTime() << "\n" << "Calories: " << Heap[1]->getNutrients()[1] << "\n" ;
+        cout << "Recipe: " << Heap[2]->getName() << "\n" << "Time to make: " << Heap[2]->getTime() << "\n" << "Calories: " << Heap[2]->getNutrients()[2] << "\n" ;
     }
 
 
