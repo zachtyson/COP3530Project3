96485b3f805ba816c523d92cc03f128f2881828b
AbdelitoCabrera <73550656+AbdelitoCabrera@users.noreply.github.com>
2022-04-17 13:55:19 -0400

    added some console gui stuff to the main, mainly just picking an option between what heap we want to use

--- main.cpp
+++ main.cpp
@@ -6,6 +6,18 @@
 #include <fstream>
 #include <chrono>
 using namespace std;
+
+bool number(const string& string)
+{
+    for (char const i : string) {
+        if (std::isdigit(i) == 0) 
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main() {
     ifstream input("reduced.csv", ios::in);
     string curr;
@@ -23,18 +35,46 @@ int main() {
         //i++;
     }
 
-    minHeap timeHeap = minHeap(recipes.size());
-    minHeap caloriesHeap = minHeap(recipes.size());
-    
-    for(int i = 0; i < recipes.size(); i++)
+    cout << "Welcome to Cooking by the Book!" << endl;
+    cout << "We have over 200,000 recipes are our disposal, fit for your needs! What are you looking for today?" << endl;
+    cout << "Enter in 1 if you would like to: Get a list of the fastest recipes to cook!" << endl;
+    cout << "Enter in 2 if you would like to: Get a list of the healthiest recipes to cook!" << endl;
+
+    string option;
+    cin >> option;
+
+    if(number(option) == false)
     {
-        timeHeap.insertTime(recipes[i]);
-        caloriesHeap.insertCal(recipes[i]);
+        cout << "Oh oh you input an unrecoginzable value, please re-run Cooking by the Book!" << endl;
+        return 0;
+    }
+    else
+    {
+        if(option != 1 || option != 2)
+        {
+            cout << "Oh oh you input an unrecoginzable value, please re-run Cooking by the Book!" << endl;
+            return 0;
+        }
+    }
+    else if(option == 1)
+    {
+        minHeap Heap = minHeap(recipes.size());
+        for(int i = 0; i < recipes.size(); i++)
+        {
+            timeHeap.insertTime(recipes[i]);
+        }
+    }
+    else
+    {
+        minHeap Heap = minHeap(recipes.size());
+        for(int i = 0; i < recipes.size(); i++)
+        {
+            timeHeap.insertTime(recipes[i]);
+            caloriesHeap.insertCal(recipes[i]);
+        }
     }
 
-    timeHeap.printTop();
-    cout << "---------------------------------------------" << endl;
-    caloriesHeap.printTop();
+    Heap.printTop();
     
 //    end = std::chrono::system_clock::now();
 //    std::chrono::duration<double> elapsed_seconds = end - start;
