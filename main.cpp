f3a0d992d95f95111e77f9a49b7d1dace6cc49f9
Zachary <tysonzachary@ufl.edu>
2022-04-18 16:05:01 -0400

    Removed duplicates from CSV

--- main.cpp
+++ main.cpp
@@ -19,22 +19,13 @@ bool number(const string& string)
 }
 
 int main() {
-    ifstream input("reduced.csv", ios::in);
+    ifstream input("RecipeDataset.csv", ios::in);
     string curr;
     getline(input, curr);
     vector<Recipe *> recipes;
-    //ofstream test2;
-    //test2.open("test2.txt");
-    //int i = 0;
-//    std::chrono::time_point<std::chrono::system_clock> start, end;
-//    start = std::chrono::system_clock::now();
-    
     while (getline(input, curr)) {
         recipes.push_back(new Recipe(curr));
-        //recipes[i]->writeToFile(test2);
-        //i++;
     }
-
     cout << "Welcome to Cooking by the Book!" << endl;
     cout << "We have over 200,000 recipes are our disposal, fit for your needs! What are you looking for today?" << endl;
     cout << "Enter in 1 if you would like to: Get a list of the fastest recipes to cook!" << endl;
@@ -74,10 +65,4 @@ int main() {
     }
 
     Heap.printTop();
-    
-//    end = std::chrono::system_clock::now();
-//    std::chrono::duration<double> elapsed_seconds = end - start;
-//    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
-//    std::cout << "Finished computation at " << std::ctime(&end_time)<< "Time to Parse Data: " << elapsed_seconds.count() << "s\n";
-
 }
